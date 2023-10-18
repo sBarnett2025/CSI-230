ef6d3ab390867615abdfd4e8717a90eb2b1922e8
Samuel Barnett <samuel.barnett@mymail.champlain.edu>
2023-10-18 13:37:48 -0400

    First PowerShell Script Finished

---
+++ week8/FirstPowershell/firstpowershellscript.ps1
@@ -0,0 +1,8 @@
+﻿
+clear
+
+Get-EventLog -list
+
+$readLog = Read-Host -prompt "Please select a log to review"
+
+Get-EventLog -logName $readLog -Newest 3 | export-csv -NoTypeInformation -Path "C:\Users\champuser\CSI-230\week8\FirstPowershell\rep.csv"
