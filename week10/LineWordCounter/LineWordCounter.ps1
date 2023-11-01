2f491136b9c4e6afb7b0eae5856f8362d01ce74c
Samuel Barnett <samuel.barnett@mymail.champlain.edu>
2023-11-01 14:05:11 -0400

    LineWordCounter assignment added and completed

---
+++ week10/LineWordCounter/LineWordCounter.ps1
@@ -0,0 +1,9 @@
+﻿clear
+
+cd "C:\Users\champuser\CSI-230"
+$FilesToLookAt = Get-ChildItem -Recurse -Filter "*.bash"
+
+for ($i=0; $i -lt $FilesToLookAt.count; $i++)
+{
+    Get-Content $FilesToLookAt[$i].FullName | Measure-Object -Line -Word
+}
