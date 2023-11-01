8938556cd66ca8aa005142f0208c1098790f1ebf
Samuel Barnett <samuel.barnett@mymail.champlain.edu>
2023-11-01 13:11:12 -0400

    Adding week 10 and completed GetServiceLogs assignment

---
+++ week10/GetServiceLogs/GetServiceLogs.ps1
@@ -0,0 +1,35 @@
+﻿clear
+
+while ($true)
+{
+    $userInput = Read-Host -Prompt "Would you like to view services that are running, stopped or all? (or type quit to end the program)"
+    $validArgs = @('all', 'stopped', 'running')
+
+    if ($validArgs -eq $userInput)
+    {
+        if ($userInput -eq "all")
+        {
+            $services = Get-Service
+            $services
+        }
+        else
+        {
+            $services = Get-Service | Where-Object { $_.Status -eq $userInput }
+            $services
+        }
+    }
+    elseif ($userInput -eq "quit")
+    {
+        break;
+    }
+    else
+    {
+        Write-Host "Invalid Input, try again."
+    }
+
+}
+
+
+
+
+
